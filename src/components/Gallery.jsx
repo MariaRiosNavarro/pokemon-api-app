7b8b1cec97ac3c7974772aa365b6c14a27e0aecb
Yan Luka Keita <yanlukakeita2000@gmail.com>
2023-11-09 09:24:39 +0100

    changes

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
+import Card from "./Card";
+
 const Gallery = () => {
+  const url = "https://pokeapi.co/api/v2/pokemon/";
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setData(data.results))
+      .catch((err) => console.error("Yan, was hast du nur getan....", err));
+  }, []);
+
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <section style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
+      {data.map((item, index) => {
+        return <Card pokemon={item} key={index} />;
+      })}
+    </section>
   );
 };
 
