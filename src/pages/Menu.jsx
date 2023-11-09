c7a8f8bf4775ed5077d07aea5edf7360726c542b
Andre Wolf <Andre77242@hotmail.de>
2023-11-09 09:15:43 +0100

    Menu.jsx fetch and map working

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,8 +1,39 @@
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 const Menu = () => {
+  const [pokemonData, setPokemonData] = useState();
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/type/")
+      .then((res) => res.json())
+      .then((data) => setPokemonData(data))
+      .catch((err) => console.error("Fehler beim Fetchen der API", err));
+  }, []);
+  console.log(pokemonData);
   return (
-    <div>
-      <h2>Type</h2>
-    </div>
+    <main>
+      <section>
+        <img
+          src="./public/img/LogoTypePage.png"
+          alt="Pokemon Logo auf der Type Page"
+        />
+        <button>
+          <img src="./public/img/close.svg" alt="X for close the Menu" />
+        </button>
+      </section>
+      <h2>TYPE</h2>
+      <section className="allTypeContainer">
+        {pokemonData && pokemonData.results && (
+          <article>
+            {pokemonData.results.map((type, index) => (
+              <div key={index}>
+                <Link to={type.url}>{type.name}</Link>
+              </div>
+            ))}
+          </article>
+        )}
+      </section>
+    </main>
   );
 };
 
