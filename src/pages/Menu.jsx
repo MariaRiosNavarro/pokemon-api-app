75e0a5f70e55fd656c6edce99e319c3a89c2883c
Andre Wolf <Andre77242@hotmail.de>
2023-11-09 13:22:43 +0100

    Sort FN, HandleClick FN, HandleType FN

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -3,13 +3,73 @@ import { useEffect, useState } from "react";
 
 const Menu = () => {
   const [pokemonData, setPokemonData] = useState();
+  const [selectedTypes, setSelectedTypes] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
+
   useEffect(() => {
     fetch("https://pokeapi.co/api/v2/type/")
       .then((res) => res.json())
-      .then((data) => setPokemonData(data))
-      .catch((err) => console.error("Fehler beim Fetchen der API", err));
+      .then((data) => {
+        // Sortierung A-Z
+        const sortedResults = data.results.sort((a, b) =>
+          a.name.localeCompare(b.name)
+        );
+
+        setPokemonData({ ...data, results: sortedResults });
+      })
+      .catch((err) =>
+        console.error(
+          "Fehler beim Fetchen der API maybe the Pokemons hide in the grass :(",
+          err
+        )
+      );
   }, []);
-  console.log(pokemonData);
+  // console.log("Oh yes i catch them all",pokemonData);
+
+  const handleTypeChange = (event) => {
+    const { value, checked } = event.target;
+    if (checked) {
+      setSelectedTypes((prevTypes) => {
+        const newTypes = [...prevTypes, value];
+        // console.log("Thats my fucking type:", newTypes);
+        return newTypes;
+      });
+    } else {
+      setSelectedTypes((prevTypes) => {
+        const newTypes = prevTypes.filter((type) => type !== value);
+        // console.log("Thats my fucking type:", newTypes);
+        return newTypes;
+      });
+    }
+  };
+
+  const handleSearch = () => {
+    // Für jeden ausgewählten Typ werden Promise-Objekte erstellt.
+    const promises = selectedTypes.map((type) =>
+      // Jedes Promise-Objekt ist eine Anfrage an die Pokemon API für einen bestimmten Type.
+      fetch(`https://pokeapi.co/api/v2/type/${type}`)
+        .then((res) => res.json())
+        .then((data) => data.pokemon)
+    );
+
+    // Promise.all nimmt ein Array von Promise-Objekten entgegen und gibt ein neues Promise zurück.
+    Promise.all(promises)
+      .then((results) => {
+        // Der Code wird ausgeführt, wenn alle Promises erfolgreich wurden.
+        // 'results' enthält die Daten der Promises.
+        const flattenedResults = results.flatMap((result) => result);
+        // Das flache Array wird im State (searchResults) gesetzt, um die gefundenen Pokemon zu speichern.
+        setSearchResults(flattenedResults);
+        console.log("Search Results: flaches Array", searchResults);
+      })
+      .catch((error) => {
+        console.error(
+          "Error fetching Pokemon data, psst they hiding in the deep grass",
+          error
+        );
+      });
+  };
+
   return (
     <main>
       <section>
@@ -17,9 +77,11 @@ const Menu = () => {
           src="./public/img/LogoTypePage.png"
           alt="Pokemon Logo auf der Type Page"
         />
-        <button>
-          <img src="./public/img/close.svg" alt="X for close the Menu" />
-        </button>
+        <Link to="/">
+          <button>
+            <img src="./public/img/close.svg" alt="X for close the Menu" />
+          </button>
+        </Link>
       </section>
       <h2>TYPE</h2>
       <section className="allTypeContainer">
@@ -27,12 +89,23 @@ const Menu = () => {
           <article>
             {pokemonData.results.map((type, index) => (
               <div key={index}>
-                <Link to={type.url}>{type.name}</Link>
+                <label>
+                  <input
+                    name="typeInput"
+                    type="checkbox"
+                    value={type.name}
+                    onChange={handleTypeChange}
+                  />
+                  {type.name}
+                </label>
               </div>
             ))}
           </article>
         )}
       </section>
+      <div>
+        <button onClick={handleSearch}>Search</button>
+      </div>
     </main>
   );
 };
