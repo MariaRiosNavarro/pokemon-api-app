e36d3c7f2e62f8d83763994f9a06eb6d27cfc632
SuperCoder <propscode254@gmail.com>
2023-11-09 14:27:09 +0100

    header done

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -3,6 +3,7 @@ import PokeDetail from "../components/PokeDetail";
 import { useParams } from "react-router-dom";
 import Loading from "../components/Loading";
 import Header from "../components/Header";
+import BackIcon from "../assets/img/back.svg";
 
 const Detail = () => {
   const [onePokemon, setOnePokemon] = useState("");
@@ -28,7 +29,7 @@ const Detail = () => {
 
   return (
     <div>
-      <Header />
+      <Header svgIcon={BackIcon} href="/" />
       <PokeDetail pokemonItem={onePokemon} />
     </div>
   );
