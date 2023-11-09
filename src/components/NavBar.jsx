eead864bd4844f1f141865ed28303e010f816188
Yan Luka Keita <yanlukakeita2000@gmail.com>
2023-11-09 11:11:11 +0100

    Search Filter function

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,14 +2,37 @@ import MenuIcon from "../assets/img/menu.svg";
 import DarkmodeIcon from "../assets/img/darkmode.svg";
 
 import "./NavBar.css";
+import { useEffect, useState } from "react";
 
 const NavBar = () => {
+  const [userInput, setUserInput] = useState();
+
+  const url = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+
+  let pokemons;
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        pokemons = data.results.filter((pokemon) => {
+          return pokemon.name.includes(userInput) ? pokemon : null;
+        });
+        console.log(pokemons);
+      })
+      .catch((err) => console.error("Yan junge....", err));
+  }, [userInput]);
+
   return (
     <nav>
       <img src={MenuIcon} alt="" className="HeaderIcon" />
 
       <form action="#">
-        <input type="text" placeholder="Search Pokemon" />
+        <input
+          onChange={(e) => setUserInput(e.target.value)}
+          type="text"
+          placeholder="Search Pokemon"
+        />
       </form>
 
       <img src={DarkmodeIcon} alt="" className="HeaderIcon" />
